cf321804c9d02c8594aa0364a2ab9587ace20b0f
MAYUR PETHANI <mayurmorin@gmail.com>
2019-01-29 22:42:50 +0530

    Add files via upload

---
+++ Supervised Learning/Linear Models/linear_regression_live-master/demo.py
@@ -0,0 +1,103 @@
+#The optimal values of m and b can be actually calculated with way less effort than doing a linear regression. 
+#this is just to demonstrate gradient descent
+
+from numpy import *
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+
+def run():
+    points = genfromtxt("data.csv", delimiter=",")
+    learning_rate = 0.0001
+    initial_b = 0 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    num_iterations = 1000
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Running...")
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
+
+
+def LR():
+    # Importing the dataset
+    dataset = pd.read_csv('data.csv')
+    #display(dataset)
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 1].values
+    #display(X)
+    #display(y)
+    
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+    
+    # Feature Scaling
+    """from sklearn.preprocessing import StandardScaler
+    sc_X = StandardScaler()
+    X_train = sc_X.fit_transform(X_train)
+    X_test = sc_X.transform(X_test)
+    sc_y = StandardScaler()
+    y_train = sc_y.fit_transform(y_train)"""
+    
+    # Fitting Simple Linear Regression to the Training set
+    from sklearn.linear_model import LinearRegression
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    
+    # Predicting the Test set results
+    y_pred = regressor.predict(X_test)
+    
+    # Visualising the Training set results
+    plt.scatter(X_train, y_train, color = 'red')
+    plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+    plt.title('Salary vs Experience (Training set)')
+    plt.xlabel('Years of Experience')
+    plt.ylabel('Salary')
+    plt.show()
+    
+    # Visualising the Test set results
+    plt.scatter(X_test, y_test, color = 'red')
+    plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+    plt.title('Salary vs Experience (Test set)')
+    plt.xlabel('Years of Experience')
+    plt.ylabel('Salary')
+    plt.show()
+
+if __name__ == '__main__':
+    run()
+    LR()
